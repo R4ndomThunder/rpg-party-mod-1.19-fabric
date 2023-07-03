cac93d19427389c6d367b57e129742db0ed86216
R4ndomThunder <samu.pad@hotmail.it>
2023-07-03 13:51:50 +0200

    Init project

---
+++ src/main/java/net/r4ndomthunder/rpgpartymod/RPGPartyMod.java
@@ -0,0 +1,15 @@
+package net.r4ndomthunder.rpgpartymod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RPGPartyMod implements ModInitializer {
+	public static final String RPGPARTYMOD = "rpgpartymod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(RPGPARTYMOD);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/r4ndomthunder/rpgpartymod/RPGPartyModClient.java
@@ -0,0 +1,11 @@
+package net.r4ndomthunder.rpgpartymod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RPGPartyModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
